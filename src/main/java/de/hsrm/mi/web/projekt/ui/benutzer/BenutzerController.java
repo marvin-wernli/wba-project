59de175ab4ab881537ebac399a95013688b96aca
LarissaSina <sinaoblong@web.de>
2024-04-24 17:18:57 +0200

    Controller hinzugefügt.

---
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -0,0 +1,17 @@
+package de.hsrm.mi.web.projekt.ui.benutzer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class BenutzerController {
+    // n mit dieser zahl ersetzen
+    @GetMapping("/benutzer/{n}")
+    public String showBenutzerBearbeiten(@PathVariable("n") Long n) {
+        return "benutzerbearbeiten";
+    }
+}
