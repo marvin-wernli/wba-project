aad3b2931128e104bc4999eb806aa97193a1c5c3
Marvin Wernli <marvin.wernli@student.hs-rm.de>
2024-04-26 11:25:23 +0200

    BenutzerFormular Klasse erstellt und PostMapping in Controller

--- src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -1,11 +1,17 @@
 package de.hsrm.mi.web.projekt.ui.benutzer;
 
+import javax.naming.Binding;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class BenutzerController {
@@ -15,4 +21,15 @@ public class BenutzerController {
         model.addAttribute("userID",userID);
         return "benutzerbearbeiten";
     }
+
+    @PostMapping("/benutzer/{n}")
+    public String submitForm(@ModelAttribute("userID") BenutzerFormular form, BindingResult result, Model model) {
+        
+        if (result.hasErrors()){
+            return "error";
+        }
+        model.addAttribute("userID", form);
+        return "benutzerbearbeiten";
+    }
+    
 }
---
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
@@ -0,0 +1,48 @@
+package de.hsrm.mi.web.projekt.ui.benutzer;
+
+import java.time.LocalDate;
+
+public class BenutzerFormular {
+    private String name;
+    private String mail;
+    private String password;
+    private LocalDate birthdate;
+    
+    public BenutzerFormular(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    
+}
