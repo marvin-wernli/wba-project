376d89c85580d4e196cf98025f2bb65546fbeba2
Marvin Wernli <marvin.wernli@student.hs-rm.de>
2024-05-21 13:23:59 +0200

    BenutzerRepository Interface erstellt

--- src/main/java/de/hsrm/mi/web/projekt/entities/benutzer/Benutzer.java
+++ src/main/java/de/hsrm/mi/web/projekt/entities/benutzer/Benutzer.java
@@ -1,8 +1,21 @@
 package de.hsrm.mi.web.projekt.entities.benutzer;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import de.hsrm.mi.web.projekt.validators.GutesPasswort;
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Benutzer {
@@ -15,6 +28,39 @@ public class Benutzer {
     // TODO: version auch als generated?
     @GeneratedValue
     private long version;
+
+    @NonNull
+    @Size(min=3,max=80)
+    private String name;
+
+    @NonNull
+    @Email()
+    private String mail;
+
+    @NonNull
+    @GutesPasswort(message="{gutespasswort.fehler}")
+    private String password;
+
+    @NonNull
+    @Past()
+    @DateTimeFormat(iso=ISO.DATE)
+    private LocalDate birthdate;
+
+    @NotNull
+    @ElementCollection
+    private Set<String> likes;
+
+    @NonNull
+    @ElementCollection
+    private Set<String> dislikes;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getVersion() {
+        return version;
+    }
 
     
 }
