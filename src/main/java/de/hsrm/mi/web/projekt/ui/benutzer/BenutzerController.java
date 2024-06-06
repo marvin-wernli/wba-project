82e4775e4e118c7321e2a39f70a548c34c512368
Marvin Wernli <marvin.wernli@student.hs-rm.de>
2024-06-06 13:54:57 +0200

    benutzerliste fragments

--- src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @SessionAttributes(names = {"form","userID","maxwunsch","benutzer"})
