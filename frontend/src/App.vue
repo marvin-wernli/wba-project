54fe715e776878cff185c72fbdd3e3b32220c966
Marvin Wernli <marvin.wernli@student.hs-rm.de>
2024-06-18 14:28:56 +0200

    vue frontend befüllt & error message (Aufgabe 2)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,39 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { computed, ref, reactive} from "vue"
+
+  const info = ref("Dies ist eine Nachricht")
+
+  function error(){
+    info.value = ""
+  }
+  function hallo(){
+    info.value = "Dies ist eine Nachricht"
+  }
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<template> 
+    <body>
+        <header >
+            <div class="header">
+            <a  class="hUsr" >Benutzer</a>
+            <a  class="hUsr" >Ort</a>
+            <a  class="hUsr" >Touren</a>
+            <a >DE</a>
+            <a >NL</a>
+            <a >EN</a>
+        </div>
+        <div v-if="info != ''" class="errorText">
+            <span  >{{info}}</span>
+            <button v-on:click="error" >x</button>
+        </div>
+        <button v-on:click="hallo"> Fehlernachricht erzeugen </button>
+        </header>
+        <footer >
+            <a class="impressum" >Impressum</a>
+            <a >Kontakt</a>
+        </footer>
+    </body>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
