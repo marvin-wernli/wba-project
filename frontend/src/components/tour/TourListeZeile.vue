4cd0b06f1dab05dfea698e21c7a8fe1d0ec98b45
(Sina) Larissa Oblong <sinalarissa.oblong@student.hs-rm.de>
2024-06-19 17:25:34 +0200

    Views Vues

---
+++ frontend/src/components/tour/TourListeZeile.vue
@@ -0,0 +1,30 @@
+<!-- Zeile von der Tabelle ausgeben -->
+
+<template>
+    <tr class="userLst">
+        <td>
+            <p>{{abfahrDateTime}}</p>
+        </td>
+        <td>
+            <p>{{startOrtName}}</p>
+        </td>
+        <td>
+            <p>{{zielOrtName}}</p>
+        </td>
+        <td>
+            <p>{{distanz}}</p>
+        </td>
+        <td>
+            <p>{{plaetze}}</p>
+        </td>
+        <td>
+            <p>frei</p>
+        </td>
+        <td>
+            <p>{{preis}}</p>
+        </td>
+        <td>
+            <button>Details</button>
+        </td>
+    </tr>
+<!template>
---
+++ frontend/src/components/tour/TourenListe.vue
@@ -0,0 +1,53 @@
+<!-- Tabelle Ausgeben -->
+
+<!-- for ... mach TourListZeil.vue mit daten-->
+
+<template>
+    <div class="border">
+        <h1>Das aktuelle Mitfahrangebot...</h1>
+        <table>
+            <tr class="userLst">
+                <td>
+                    <p>Abfahrt</p>
+                </td>
+                <td>
+                    <p>von</p>
+                </td>
+                <td>
+                    <p>nach</p>
+                </td>
+                <td>
+                    <p>Entfernung</p>
+                </td>
+                <td>
+                    <p>Plätze</p>
+                </td>
+                <td>
+                    <p>frei</p>
+                </td>
+                <td>
+                    <p>Preis</p>
+                </td>
+                <td>
+                    <button>Details</button>
+                </td>
+            </tr>
+            <TourListZeile v-bind="tourliste" />
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const touren = defineProps<{
+    abfahrDateTime: string
+    preis: number
+    plaetze: number
+    startOrtName: string
+    zielOrtName: string
+    anbieterName: string
+    distanz: number
+    info: string
+}
+
+</script>
